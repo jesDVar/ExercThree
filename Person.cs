63594e00de9618d560c154b94fa6ff2356ec9bd8
Jesse Varis <jessevaris@hotmail.com>
2021-12-10 02:36:03 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercThree
+{
+    class Person
+    {
+
+        private int age;
+        private string fName;
+        private string lName;
+        private int height;
+        private int weight;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = 10; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExercThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+
+}
